2e6221d292bd581e867259594ce658992a13bba2
Jose Cepero <ceperojose54@gmail.com>
2023-08-30 20:17:48 -0700

    added code for gets and comment by post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,58 @@
 // implement your posts router here
+const express = require('express')
+
+const Posts = require('./posts-model')
+
+const router = express.Router()
+
+router.get('', async (req, res) =>{
+    try{
+        const postArr = await Posts.find();
+        console.log(postArr)
+        res.json(postArr)
+    }catch(err){
+        res.status(500).json({ message: "The posts information could not be retrieved" })
+    }
+})
+
+router.get('/:id', async (req, res) =>{
+    try{
+        const { id } = req.params;
+        const postById = await Posts.findById(id)
+        console.log(postById)
+        if(postById)
+            res.json(postById)
+        else 
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+    }catch(err){
+        res.status(500).json({ message: "The post with the specified ID does not exist" })
+    }
+})
+
+router.post('/', async (req, res) =>{
+    try{
+        console.log('post ran')
+        res.json(req)
+    }catch(err){
+        res.status(500).json({ message: "The post with the specified ID does not exist" })
+    }
+})
+
+router.get('/:id/comments', async (req,res) =>{
+    try{
+        const { id } = req.params;
+        const postComments = await Posts.findPostComments(id)
+        
+        if (postComments.length>0){
+            res.json(postComments)
+        }
+        else{
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        }
+    }catch(err){
+        res.status(500).json({ message: "The comments information could not be retrieved" })
+    }
+})
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
 // implement your server here
+const express = require('express')
+
+
+const postsRouter = require('./posts/posts-router')
+const server = express();
 // require your posts router and connect it here
+
+server.use('/api/posts',postsRouter)
+server.use(express.json());
+
+server.get('/',(req, res)=>{
+    res.json({message:'this shit is working'})
+})
+
+
+module.exports = server;
