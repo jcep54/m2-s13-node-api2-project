234f9315bf6d211afbbe1706716f81c509ddecb7
Jose Cepero <ceperojose54@gmail.com>
2023-09-05 18:26:04 -0400

    finished server requests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -29,12 +29,71 @@ router.get('/:id', async (req, res) =>{
     }
 })
 
-router.post('/', async (req, res) =>{
+router.post('/',  (req, res) =>{
+    const { title, contents } = req.body
+    if (!title || !contents){
+        res.status(400).json({
+            message:'Please provide title and contents for the post'
+        })
+    }else {
+        Posts.insert({ title, contents})
+            .then( async stuff =>{
+                const postById = await Posts.findById(stuff.id)
+                console.log(postById)
+                res.status(201).json(postById)
+            })
+            .catch(err =>{
+                res.status(500).json({
+                    message:'There was an error while saving the post to the database',
+                    err: err.message,
+                    stack:err.stack,
+                })
+            })
+    }
+})
+
+router.put('/:id', async (req, res) =>{
+    const { id } = req.params;
+    const { title, contents } = req.body
+    const validId = await Posts.findById(id)
+    if(!validId){
+        res.status(404).json({ message: "The post with the specified ID does not exist" })
+    }else if (!title|| !contents){
+        res.status(400).json({ message: "Please provide title and contents for the post" })
+    }else{
+    Posts.update(id, {title,contents})
+        .then(async () =>{
+            const updatedItem = await Posts.findById(id)
+            res.json(updatedItem)
+        })
+        .catch(err =>{
+            res.status(500).json({
+                message:'The post information could not be modified',
+                err:err.message,
+                stack:err.stack
+            })
+        })
+    }
+
+})
+
+router.delete('/:id', async (req, res)=>{
     try{
-        console.log('post ran')
-        res.json(req)
-    }catch(err){
-        res.status(500).json({ message: "The post with the specified ID does not exist" })
+        const { id } = req.params
+        const deletedId = await Posts.findById(id)
+        if(deletedId){
+            Posts.remove(id)
+            .then( () =>{
+               res.json(deletedId) 
+            })
+
+            
+        }else{
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        }
+
+    }catch{
+        res.status(500).json({ message: "The post could not be removed" })
     }
 })
 
--- api/server.js
+++ api/server.js
@@ -5,9 +5,9 @@ const express = require('express')
 const postsRouter = require('./posts/posts-router')
 const server = express();
 // require your posts router and connect it here
-
-server.use('/api/posts',postsRouter)
 server.use(express.json());
+server.use('/api/posts',postsRouter)
+
 
 server.get('/',(req, res)=>{
     res.json({message:'this shit is working'})
